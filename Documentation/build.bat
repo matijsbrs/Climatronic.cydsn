e63d5244516d3dd4778dab99934adff607258458
Matijs Behrens <matijs.behrens@outlook.com>
2021-10-26 21:36:27 +0200

    FSM updates

--- Documentation/build.bat
+++ Documentation/build.bat
@@ -1,1 +1,3 @@
 dot -Tsvg "FSM - Climate - Ventilation control.gv" -o FSM.Climate.svg
+dot -Tsvg "FSM - Climate - Temperature control.gv" -o FSM.Temperature.svg
+dot -Tsvg "FSM - Thermostat.gv" -o FSM.Thermostat.svg
